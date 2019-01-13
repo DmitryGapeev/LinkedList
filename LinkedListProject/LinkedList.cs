23f451f5bb8f425c816b67291dfc27d6cd0574e4
Дмитрий Гапеев <gapeevdirog@gmail.com>
2019-01-13 20:33:30 +0300

    Add project files.

---
+++ LinkedListProject/LinkedList.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace AlgorithmsDataStructures
+{
+
+  public class Node
+  {
+    public int value;
+    public Node next;
+    public Node(int _value) { value = _value; }
+  }
+
+  public class LinkedList
+  {
+    public Node head;
+    public Node tail;
+
+    public LinkedList()
+    {
+      head = null;
+      tail = null;
+    }
+
+    public void AddInTail(Node _item)
+    {
+      if (head == null) head = _item;
+      else tail.next = _item;
+      tail = _item;
+    }
+
+    public Node Find(int _value)
+    {
+      Node node = head;
+      while (node != null)
+      {
+        if (node.value == _value) return node;
+        node = node.next;
+      }
+      return null;
+    }
+
+    public List<Node> FindAll(int _value)
+    {
+      List<Node> nodes = new List<Node>();
+
+      Node node = head;
+      while (node != null)
+      {
+        if (node.value == _value)
+          nodes.Add(node);
+
+        node = node.next;
+      }
+
+      return nodes;
+    }
+
+    public bool Remove(int _value)
+    {
+      bool isDeleted = false;
+      if (head == null)
+        isDeleted = false;
+      else
+      {
+        Node node = head;
+        Node prevNode = null;
+        while (node != null && !isDeleted)
+        {
+          if (node.value == _value)
+          {
+            if(prevNode!=null)
+              prevNode.next = node.next;
+            else
+              head = node.next;
+
+            isDeleted = true;
+          }
+          else
+            prevNode = node;
+
+          node = node.next;
+        }
+
+        if(node == null)
+          tail = prevNode;
+      }
+
+      return isDeleted;
+    }
+
+    public void RemoveAll(int _value)
+    {
+      Node node = head;
+      Node prevNode = null;
+      while (node != null)
+      {
+        if (node.value == _value)
+        {
+          if(prevNode != null)
+            prevNode.next = node.next;
+          else
+          {
+            head = node.next;
+          }
+        }
+        else
+          prevNode = node;
+
+        node = node.next;
+      }
+
+      tail = prevNode;
+    }
+
+    public void Clear()
+    {
+      Node node = head;
+      head = null;
+      tail = null;
+      while (node != null)
+      {
+        Node nextNode = node.next;
+        node = null;
+        node = nextNode;
+      }
+    }
+
+    public int Count()
+    {
+      int count = 0;
+      var node = head;
+      if (node != null)
+      {
+        while (node != null)
+        {
+          count++;
+          node = node.next;
+        }
+      }
+
+      return count;
+    }
+
+    public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+    {
+      if (head == null)
+        AddInTail(_nodeToInsert);
+      else  if (_nodeAfter == null)
+      {
+        _nodeToInsert.next = head;
+        head = _nodeToInsert;
+      }
+      else if (tail == _nodeAfter)
+      {
+        tail.next = _nodeToInsert;
+        tail = _nodeToInsert;
+      }
+      else
+      {
+        _nodeToInsert.next = _nodeAfter.next;
+        _nodeAfter.next = _nodeToInsert;
+      }
+    }
+
+  }
+}
---
+++ LinkedListProject/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestClearLinkedList();
+      TestRemoveElement();
+      TestFindElement();
+      TestFindElements();
+      TestInsertAfter();
+
+      Console.ReadKey();
+    }
+
+    static void TestClearLinkedList()
+    {
+      LinkedList list = new LinkedList();
+      list.AddInTail(new Node(20));
+      list.AddInTail(new Node(24));
+      list.AddInTail(new Node(12));
+      list.AddInTail(new Node(77));
+      list.AddInTail(new Node(24));
+
+      Console.WriteLine("test clear");
+
+      Console.WriteLine("count before clear " + list.Count());
+      Console.WriteLine("head before clear " + list.head.value);
+      Console.WriteLine("tail before clear " + list.tail.value);
+      list.Clear();
+      Console.WriteLine("count after clear " + list.Count());
+      Console.WriteLine("head after clear " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail after clear " + (list.head == null ? "null" : list.head.value.ToString()));
+
+      Console.WriteLine(new string('=', 30));
+    }
+
+    static void TestRemoveElement()
+    {
+
+      LinkedList list = new LinkedList();
+      list.AddInTail(new Node(20));
+      list.AddInTail(new Node(24));
+      list.AddInTail(new Node(12));
+      list.AddInTail(new Node(77));
+      list.AddInTail(new Node(24));
+
+      Console.WriteLine("test remove");
+
+      Console.WriteLine("delete 12");
+      Console.WriteLine(string.Format("count before delete {0}", list.Count()));
+      list.Remove(12);
+      Console.WriteLine(string.Format("count after delete {0}", list.Count()));
+
+      Console.WriteLine("delete 24");
+      Console.WriteLine(string.Format("count before delete {0}", list.Count()));
+      list.RemoveAll(24);
+      Console.WriteLine(string.Format("count after delete {0}", list.Count()));
+      Console.WriteLine("head = " + list.head.value);
+      Console.WriteLine("tail = " + list.tail.value);
+
+      Console.WriteLine("delete 20");
+      Console.WriteLine(string.Format("count before delete {0}", list.Count()));
+      list.Remove(20);
+      Console.WriteLine(string.Format("count after delete {0}", list.Count()));
+      Console.WriteLine("head = " + list.head.value);
+      Console.WriteLine("tail = " + list.tail.value);
+
+      Console.WriteLine("delete 20");
+      Console.WriteLine(string.Format("count before delete {0}", list.Count()));
+      list.Remove(77);
+      Console.WriteLine(string.Format("count after delete {0}", list.Count()));
+      Console.WriteLine("head = " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail = " + (list.tail == null ? "null" : list.tail.value.ToString()));
+
+      Console.WriteLine(new string('=', 30));
+    }
+
+    static void TestFindElement()
+    {
+      LinkedList list = new LinkedList();
+      list.AddInTail(new Node(20));
+      list.AddInTail(new Node(24));
+      list.AddInTail(new Node(12));
+      list.AddInTail(new Node(77));
+      list.AddInTail(new Node(24));
+
+      Console.WriteLine("test find");
+
+      Console.WriteLine("find 20");
+      var result = list.Find(20);
+      Console.WriteLine("result " + result.value);
+
+      Console.WriteLine();
+
+      Console.WriteLine("find 12");
+      result = list.Find(12);
+      Console.WriteLine("result " + result.value);
+
+      Console.WriteLine();
+
+      Console.WriteLine("find 24");
+      result = list.Find(24);
+      Console.WriteLine("result " + result.value);
+
+      Console.WriteLine("find 100");
+      result = list.Find(100);
+      Console.WriteLine("result " + (result == null ? "null" : result.value.ToString()));
+
+      Console.WriteLine(new string('=', 30));
+    }
+
+    static void TestFindElements()
+    {
+      LinkedList list = new LinkedList();
+      list.AddInTail(new Node(20));
+      list.AddInTail(new Node(24));
+      list.AddInTail(new Node(12));
+      list.AddInTail(new Node(77));
+      list.AddInTail(new Node(24));
+
+      Console.WriteLine("test find all");
+
+      Console.WriteLine("find 20");
+      var result = list.FindAll(20);
+      foreach (var item in result)
+        Console.WriteLine(item.value);
+
+      Console.WriteLine("find 24");
+      result = list.FindAll(24);
+      foreach (var item in result)
+        Console.WriteLine(item.value);
+
+      Console.WriteLine(new string('=', 30));
+
+    }
+
+    static void TestInsertAfter()
+    {
+      Console.WriteLine("test insert after");
+      var list = new LinkedList();
+      Console.WriteLine("insert 10");
+      Console.WriteLine("count before insert = " + list.Count());
+      Console.WriteLine("head before insert =  "  + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail before insert =  " + (list.tail == null ? "null" : list.tail.value.ToString()));
+      list.InsertAfter(null, new Node(10));
+      Console.WriteLine("count after insert = " + list.Count());
+      Console.WriteLine("head after insert =  " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail after insert =  " + (list.tail == null ? "null" : list.tail.value.ToString()));
+
+      Console.WriteLine();
+
+      Console.WriteLine("insert 20 after 10");
+      Console.WriteLine("count before insert = " + list.Count());
+      Console.WriteLine("head before insert =  " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail before insert =  " + (list.tail == null ? "null" : list.tail.value.ToString()));
+      list.InsertAfter(list.head, new Node(20));
+      Console.WriteLine("count after insert = " + list.Count());
+      Console.WriteLine("head after insert =  " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail after insert =  " + (list.tail == null ? "null" : list.tail.value.ToString()));
+
+      Console.WriteLine();
+
+      Console.WriteLine("insert 30 before head");
+      Console.WriteLine("count before insert = " + list.Count());
+      Console.WriteLine("head before insert =  " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail before insert =  " + (list.tail == null ? "null" : list.tail.value.ToString()));
+      list.InsertAfter(null, new Node(30));
+      Console.WriteLine("count after insert = " + list.Count());
+      Console.WriteLine("head after insert =  " + (list.head == null ? "null" : list.head.value.ToString()));
+      Console.WriteLine("tail after insert =  " + (list.tail == null ? "null" : list.tail.value.ToString()));
+
+      Console.WriteLine(new string('=', 30));
+      
+    }
+
+    static LinkedList UnionLinkedLists(LinkedList firstLink, LinkedList secondList)
+    {
+      if (firstLink.Count() != secondList.Count())
+        return null;
+
+      Node nodeFirstList = firstLink.head;
+      Node nodeSecondList = secondList.head;
+      LinkedList newList = new LinkedList();
+
+      while (nodeFirstList != null && nodeSecondList != null)
+      {
+        int newNodeValue = nodeSecondList.value + nodeSecondList.value;
+        newList.AddInTail(new Node(newNodeValue));
+        nodeFirstList = nodeFirstList.next;
+        nodeSecondList = nodeSecondList.next;
+      }
+
+      return newList;
+    }
+  }
+}
